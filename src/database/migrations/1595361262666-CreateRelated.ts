09abb1444ad1d1ae2761fe2ea88a699ed880e0b7
Fernando <ffonseca1986@gmail.com>
2020-07-23 18:15:54 -0300

    CRUD e importacao csv

---
+++ src/database/migrations/1595361262666-CreateRelated.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class CreateRelated1595361262666 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        name: 'TransactionCategory',
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategory');
+  }
+}
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,8 +11,66 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const incomes = await this.find({
+      where: { type: 'income' },
+    });
+
+    const totalIncome = incomes.reduce((sum, incomeValue) => {
+      return sum + Number(incomeValue.value);
+    }, 0);
+
+    const outcomes = await this.find({
+      where: { type: 'outcome' },
+    });
+
+    const totalOutcome = outcomes.reduce((sum, outcomeValue) => {
+      return sum + Number(outcomeValue.value);
+    }, 0);
+
+    const fullTotal = totalIncome - totalOutcome;
+
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total: fullTotal,
+    };
+
+    return balance;
   }
 }
 
 export default TransactionsRepository;
+
+/* Solução utilizando Switch Case
+public async getBalance(): Promise<Balance> {
+  const transactions = await this.find();
+
+  const { income, outcome } = transactions.reduce(
+    (accumulator, transaction) =>{
+      switch (transaction.type){
+        case 'income':
+          accumulator.income += transaction.value;
+          break;
+
+        case 'outcome':
+          accumulator.outcome += transaction.value;
+          break;
+
+          default:
+            break;
+      }
+      return accumulator;
+    },
+
+    {
+      income:0,
+      outcome:0,
+      total:0
+    },
+
+      }
+    }
+  )
+
+
+*/
